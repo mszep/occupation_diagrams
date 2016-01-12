5f74471dd275e5974bbdcb75835d00cbd417c4fd
Mark Szepieniec <mszepien@gmail.com>
2016-01-12 17:55:10 +0000

    Make compatible with newest version of the Diagrams package

--- OccupationDiagrams.hs
+++ OccupationDiagrams.hs
@@ -19,7 +19,7 @@ occSingleUp = levelBar <> upArrow
 occSingleDown = levelBar <> downArrow
 occDouble = levelBar <> upArrow <> downArrow
 
-fromChar :: Char -> Diagram B R2
+fromChar :: Char -> Diagram B
 fromChar '|' = occEmpty
 fromChar '<' = occSingleDown
 fromChar '>' = occSingleUp
@@ -29,14 +29,13 @@ fromChar _   = undefined
 myVcat = vcat' (with & sep .~ vSep & catMethod .~ Distrib)
 myHcat = hcat' (with & sep .~ hSep & catMethod .~ Distrib)
 
---occDiagram :: [Char] -> Diagram B R2
 occDiagram cs = myVcat $ reverse $ map fromChar cs #
                   lineCap LineCapRound #
-                  lw 0.03
+                  lw 1.0
 
 occDiagrams ss = (myHcat $ map occDiagram ss) # centerXY # pad 1.1
 
-occDiagramsFromFile :: FilePath -> IO (Diagram SVG R2)
+occDiagramsFromFile :: FilePath -> IO (Diagram SVG)
 occDiagramsFromFile file = do
   f <- readFile file
   return $ occDiagrams $ lines f
